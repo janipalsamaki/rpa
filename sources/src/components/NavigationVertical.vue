0b83565e4401b0b136d965b78de06ef15a7116cd
Jani Palsamäki <jani.palsamaki@gmail.com>
2020-03-02 09:56:33 +0200

    Remove company links

--- sources/src/components/NavigationVertical.vue
+++ sources/src/components/NavigationVertical.vue
@@ -5,41 +5,8 @@
         <b-img style="margin:0;padding:15%;" :src="require('@/assets/img/ROBOTFW_Mark_White_LOW_cropped.png')" class="img-fluid" alt="Robot Frameworkg logo"/>
         
   </b-navbar-brand>
-  <p style="text-transform: none;font-size: 0.5em;color:#999;text-align:left;padding:10%;">The Robot Framework Foundation maintains RPA functionality as part of ongoing Robot Framework development. <br><br>The initial development of Robot Framework's RPA functionality was sponsored by five Foundation member companies.</p>
-    <b-nav-item :href="item.href" v-for="(item,index) in pages" :key="index">{{item.title}}</b-nav-item>
+  <p style="text-transform: none; color:#999; margin: 0.8em;">The Robot Framework Foundation maintains RPA functionality as part of ongoing Robot Framework development.</p>
 </b-nav>
 
 </section>
 </template>
-
-<script>
-export default {
-  // props: ["pages"],
-  data() {
-    return {
-      pages: [
-        {
-          title: "Siili Solutions",
-          href: "https://www.siili.com"
-        },
-        {
-          title: "Knowit",
-          href: "https://www.knowit.fi"
-        },
-        {
-          title: "VALA Group",
-          href: "https://www.valagroup.com"
-        },
-        {
-          title: "Qentinel",
-          href: "https://qentinel.com"
-        },
-        {
-          title: "Eficode",
-          href: "https://www.eficode.com"
-        }
-      ]
-    };
-  }
-};
-</script>
